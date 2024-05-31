f0c027a4547128ccb5b93e1dede56a58db827988
Gabriel Fournier <gabriel.fournier@iex.ec>
2024-05-31 15:09:05 +0200

    remove useless files

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { IExecWeb3mail } from './web3mail/IExecWeb3mail.js';
-export * from './web3mail/types.js';
-export { getWeb3Provider } from './utils/getWeb3Provider.js';
--- src/utils/generateUniqueId.ts
+++
@@ -1,6 +0,0 @@
-import { randomBytes } from '@ethersproject/random';
-import { hexlify } from '@ethersproject/bytes';
-
-export function generateSecureUniqueId(length) {
-  return hexlify(randomBytes(length));
-}
